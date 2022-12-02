5cc4aeff3243ac91228a9dee10f3a7a0e8063d33
mehdi mouloudj <mehdi.mouloudj@outlook.fr>
2022-12-02 15:34:10 +0100

    some refacto

--- infra/resources.py
+++ infra/resources.py
@@ -4,4 +4,3 @@ OUTPUT_IMAGES_BUCKET_NAME = "stable-diffusion-output-bucket"
 MODEL_DATA_BUCKET_NAME="stable-diffusion-ml-model-bucket"
 INFERENCE_SAGEMAKER_ENDPOINT_NAME="huggingface-pytorch-inference"
 MODEL_DATA_ARCHIVE_NAME = "sdv1-4_model.tar.gz"
- 
